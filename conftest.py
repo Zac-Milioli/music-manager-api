3c19e3b5bd02c2d098db26572ea91cc787163fd7
Zac Milioli <zac.milioli@agcapital.com.br>
2024-11-14 15:21:10 -0300

    update: atualização do formato do banco de físico para em memória (temporário)

--- conftest.py
+++ conftest.py
@@ -2,8 +2,6 @@ from main import app
 from fastapi.testclient import TestClient
 from src.models import table_registry
 from sqlalchemy import create_engine
-from glob import glob
-from os import remove
 import pytest
 
 @pytest.fixture()
@@ -12,14 +10,7 @@ def client():
 
 @pytest.fixture()
 def connection():
-    database_path = r'tests/test_database.db'
-
-    # Remove o arquivo database para testar com um arquivo novo
-    globed = glob(database_path)
-    if len(globed) == 1:
-        remove(database_path)
-
     # Cria o database e inicializa a engine
-    engine = create_engine(f"sqlite:///{database_path}")
+    engine = create_engine(f"sqlite:///:memory:")
     table_registry.metadata.create_all(engine)
     return engine
