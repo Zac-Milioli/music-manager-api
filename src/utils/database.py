98c2e428229a6d96ee5d5105d3d3a6bc548db1bd
Zac Milioli <zac.milioli@agcapital.com.br>
2024-11-21 14:32:51 -0300

    teste: verificando novo método de instanciar dabatase em prol do CI sem env

--- src/utils/database.py
+++ src/utils/database.py
@@ -4,10 +4,8 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from src.utils.settings import Settings
 
-engine = create_engine(Settings().DATABASE_URL)
 
-
-def get_session():
+def get_session(engine = create_engine(Settings().DATABASE_URL)):
     "Cria uma sessão para acesso temporário ao banco"
     with Session(engine) as session:
         yield session
